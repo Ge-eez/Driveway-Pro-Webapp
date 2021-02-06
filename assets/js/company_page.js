1f59bda6a624e5e9463ac69eac4242936dcb0131
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-02-07 00:40:54 +0300

    js code for company page

---
+++ assets/js/company_page.js
@@ -0,0 +1,15 @@
+
+function openLink(e, id){
+    var i,tab_content,tablinks;
+    tab_content = document.getElementsByClassName("tab-content");
+    for(i = 0; i<tab_content.length; i++){
+        tab_content[i].style.display = 'none';
+    }
+    
+    tablinks = document.getElementsByClassName("tab-links");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(id).style.display = "flex";
+    e.currentTarget.className += " active";
+}
