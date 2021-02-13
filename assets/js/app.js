dee4fd0862d0afa4c1b2aecc1bf9c0cb2d0cfc49
Eden Berhan Alem <edenberhanalem@gmail.com>
2021-02-14 02:10:24 +0300

    Retrieved and displayed nearby places with the specific details

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,20 @@
 const parkBtn = document.querySelector('.parkButton');
 const nearbylists = document.querySelector('.nearbylists');
+const display = document.querySelector('.display');
+const getdir = document.querySelector('.getdir');
 const map = document.querySelector('#myMap');
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+const modal_content = document.querySelector(".content");
 
 
 let DB;
 document.addEventListener('DOMContentLoaded', () => {
-    let nearby = indexedDB.open('nearby', 1);
-    nearby.onsuccess = function() {
-        console.log(`Database is ready`);
-        DB = nearby.result;
-
-        console.log(DB);        
-    }
-
-    nearby.onerror = function() {
-        console.log(`An error occured`);
-    }
-    
-    // let companiesData = JSON.parse(company);
-    // alert(companiesData[0].name);
-    // alert(companiesData[0].email);
-    // alert(companiesData[1].name);
-    // alert(companiesData[1].email);
-
-    const companydata = [
-        { active: true, charge: 10, closes_at: 12, floor: 1, name: "Eliana", opens_at: 1, rank_per_floor: 4, slots_per_floor: 6, latitude: 9.07, longitude: 38.77 },
-        { active: true, charge: 10, closes_at: 12, floor: 0, name: "Tk", opens_at: 1, rank_per_floor: 4, slots_per_floor: 6, latitude: 9.09, longitude: 38.79},
-        { active: true, charge: 10, closes_at: 12, floor: 0, name: "Attris", opens_at: 1, rank_per_floor: 4, slots_per_floor: 6, latitude: 9.06, longitude: 38.77}
-      ];
-
-    //on upgrade or after version is changed
-    nearby.onupgradeneeded = function(e) {
-        console.log(`On upgrade`);
-        let db = e.target.result;
-
-        let objectStore = db.createObjectStore('nearby', {keyPath:'id', autoIncrement: true});
-
-        objectStore.createIndex('active', 'active', {unique: false});
-        objectStore.createIndex('closes_at', 'closes_at', {unique: false});
-        objectStore.createIndex('floor', 'floor', {unique: false});
-        objectStore.createIndex('name', 'name', {unique: false});
-        objectStore.createIndex('opens_at', 'opens_at', {unique: false});
-        objectStore.createIndex('rank_per_floor', 'rank_per_floor', {unique: false});
-        objectStore.createIndex('slots_per_floor', 'slots_per_floor', {unique: false});
-
-
-        objectStore.transaction.oncomplete = function(e) {
-            // Store values in the newly created objectStore.
-            var objectStore = db.transaction("nearby", "readwrite").objectStore("nearby");
-            companydata.forEach(function(company) {
-              objectStore.add(company);
-            });
-          };
-        console.log(`Its ready`);
-
-    }
+    let CompanyDB = indexedDB.open("companies", 1);
+    CompanyDB.onsuccess = function () {
+        console.log('Database Ready');
+        DB = CompanyDB.result;
+    };
 
     parkBtn.addEventListener("click", park);
     function park(e) {
@@ -63,18 +23,25 @@ document.addEventListener('DOMContentLoaded', () => {
         // where R is earth’s radius (mean radius = 6,371km);
         // note that angles need to be in radians to pass to trig functions!
         navigator.geolocation.getCurrentPosition(locationHandler);
+        // DB.transaction("users").objectStore("users").get(localStorage.getItem("user")).onsuccess = function(event) {
+        //     console.log(event.target.result.name);
+        // }
 
         function locationHandler(position){
             while(nearbylists.firstChild) { nearbylists.removeChild(nearbylists.firstChild) }
 
-            let objectStore = DB.transaction("nearby").objectStore("nearby");
-
+            let objectStore = DB.transaction("companies").objectStore("companies");
+            
             objectStore.openCursor().onsuccess = function(e) {
                 let cursor = e.target.result;
                 
                 if (cursor) {
                     const li = document.createElement('li');
                     li.className = 'nearby_collections';
+
+                    const link = document.createElement('a');
+                    link.className = 'details';
+                    link.innerHTML = '<p>more details</p>';
 
                     let lat = position.coords.latitude;
                     let lng = position.coords.longitude;
@@ -83,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     let dLat = (cursor.value.latitude - lat).toRad();
                     let dLon = (cursor.value.longitude - lng).toRad();
                     let lat1 = position.coords.latitude.toRad();
-                    let lat2 = 9.07.toRad();
+                    let lat2 = cursor.value.latitude.toRad();
 
                     let a = Math.sin(dLat/2) * Math.sin(dLat/2) +
                             Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
@@ -92,40 +59,75 @@ document.addEventListener('DOMContentLoaded', () => {
                     console.log(distance);
 
                     // Identify nearby parking place within a distance of 5 km
-                    if (distance <= 5 && (cursor.value.active === true)) {
-                        nearbylists.appendChild(li);   
-
-                        // li.style.display ='block';
-                        nearbylists.backgroundColor = '#E76900';
-                        li.style.textAlign = 'center';
-                        // li.style.backgroundColor = '#E76900';
-                        li.style.listStyle = 'none';
-                        li.style.fontSize = '3rem';
-            
+                    if (distance <= 5 && (cursor.value.active_slots > 0)) {
                         // Create text node and append it
+                        li.setAttribute('data-id', cursor.value.email);
                         li.appendChild(document.createTextNode(cursor.value.name));
-                        
+                        li.appendChild(document.createTextNode(cursor.value.email));
+
+                        nearbylists.appendChild(li);   
+                        li.appendChild(link);
+
+                        nearbylists.style.height = '100vh';
+                        nearbylists.style.padding = '0';                        
+
+                        map.style.display = 'none';
+                        parkBtn.style.display = 'none';
+                        getdir.style.padding = '0';            
+                       
+                        console.log(cursor.value.name);
 
                     }
-                    cursor.continue();                   
+                    cursor.continue();                  
                     
             
                 }
-        // console.log(document.getElementsByClassName('.nearbylists').style.color);
-        
-        // document.getElementsByClassName('.nearby_collections').style.display ='block';
-        // document.getElementsByClassName('.nearby_collections').style.size ='80px';
-        // map.style.display = 'none';
-        // console.log(document.querySelectorAll('.nearbylists').style.color);      
-
-            
-            
-
                 
             }
         }
     
     }
+
+    nearbylists.addEventListener('click', details);
+
+    function details(e) {
+        if (e.target.parentElement.classList.contains('details')) {
+                // get the task id
+                let taskID = (e.target.parentElement.parentElement.getAttribute('data-id'));
+                
+                modal_content.appendChild(document.createTextNode(taskID));
+
+                // const detail = document.createElement('li');
+                // detail.className = 'details_info';
+                // detail.appendChild(document.createTextNode(taskID));
+                // detail.appendChild(document.createTextNode(taskID));
+                // modal_content.appendChild(detail);
+                          
+                modal_content.style.display = "block";
+                modal.style.display = "block";
+                // const closeBtn = document.createElement('a');
+                // closeBtn.className = 'close';
+                // closeBtn.innerHTML = '<span class="close">&times;</span>';               
+                
+                
+                console.log(taskID);
+
+                span.onclick = function() {
+                    modal.style.display = "none";
+                    modal_content.innerHTML = '';
+                }
+
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                      modal.style.display = "none";
+                      modal_content.innerHTML = '';
+                    }
+                }
+        }
+
+    }
+
+
 
 
 });
