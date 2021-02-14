2b9f84c68cf776be7d2c25b83ce443efcafc4ea3
Mintesnot Bezabhi <mintesnotbezabhi99@gmail.com>
2021-02-14 03:05:57 +0300

    some functionality for the company page

--- assets/js/company_page.js
+++ assets/js/company_page.js
@@ -2,13 +2,49 @@ const parkingOfficer = document.querySelector(".officers-list");
 const addBtn = document.getElementById("add-btn");
 const cancelIcon = document.getElementById("cancel");
 const form = document.getElementById("pop-up");
+const keyEmail = localStorage.getItem('company');
+const email = "contact@tk.com";
+const formUpdate = document.getElementById("update-profile");
 
 //form inputs
+const companyInput = document.getElementById("company");
+const emailInput = document.getElementById("inputEmail");
+const latitudeInput = document.getElementById("latitude");
+const longtiudeInput = document.getElementById("longtiude");
+const chargeInput = document.getElementById("charge");
+const slotsInput = document.getElementById("slots");
+// 
 const officerFName = document.getElementById("officer-fName");
 const officerLName = document.getElementById("officer-lName");
 const officerEmail = document.getElementById("officer-Email");
 const officerPhone = document.getElementById("officer-phone");
 const officerSex = document.getElementById("officer-sex");
+//company profil data text
+const companyText = document.getElementById("companyText");
+const emailText = document.getElementById("emailText");
+const locationText = document.getElementById("locationText");
+const chargeText = document.getElementById("chargeText");
+const slotsText = document.getElementById("slotsText");
+
+let DB;
+document.addEventListener("DOMContentLoaded",()=>{
+    let companyDB = indexedDB.open("companies",2)
+    companyDB.onsuccess = function(){
+        DB = companyDB.result;
+        displayProfile();
+        display_parking_officer();
+    }    
+    companyDB.onupgradeneeded = function(e){
+        
+        let db = e.target.result;
+        let objectStore = db.createObjectStore('parkingOfficer', { keyPath: 'companyEmail' });
+        objectStore.createIndex('parkingOfficer', ['name','companyEamil'], { unique: true });
+        console.log("created store");
+    }
+
+   
+});
+
 
 function openLink(e, id){
     var i,tab_content,tablinks;
@@ -24,6 +60,111 @@ function openLink(e, id){
     document.getElementById(id).style.display = "block";
     e.currentTarget.className += " active";
 }
+// profile part
+ 
+form.addEventListener('submit', add_parking_officer)
+var i = 0;
+function add_parking_officer(e){
+    e.preventDefault(); 
+    
+    let poDB = DB.transaction(["parkingOfficer"],'readwrite')
+    let objStore = poDB.objectStore('parkingOfficer');
+    
+    let poInputs = {
+        name: officerFName.value + " " + officerLName.value,
+        phoneNumber: officerPhone.value,
+        sex: officerSex.value,
+        companyEmail: officerEmail.value
+    }
+
+    let result = objStore.add(poInputs);
+    result.onsuccess = ()=>{}
+    result.onerror = (e)=>{console.log(e)}
+    poDB.oncomplete = ()=>{
+        console.log("new task added");
+        parkingOfficer.innerHTML = "";
+        display_parking_officer();
+    }
+
+    form.style.display = "none";
+    document.getElementById("add-btn").removeAttribute("disabled");
+   
+}
+function display_parking_officer(){
+    let store = DB.transaction(['parkingOfficer']).objectStore('parkingOfficer');
+    store.openCursor().onsuccess = function(e){
+        let cursor = e.target.result;
+        if(cursor){
+            let listItem = `
+            <ul class="list-inline row list-item">
+                <li class="col-1 ml-0 pl-5 ">
+                    <input class="input-group" type="checkbox" value="" id="selectAll" />
+                </li>
+                <li class="col-2">${cursor.value.name}</li>
+                <li class="col-2 ">${cursor.value.phoneNumber}</li>
+                <li class="col-3">${cursor.value.companyEmail}</li>
+                <li class="col-2">${cursor.value.sex}</li>
+                <li class="col-2 ">
+                    <i class="fa fa-remove mr-2"></i>  &nbsp;<a href="#"><i class="fa fa-edit"></i> </a>
+                </li>
+            </ul>`;
+            parkingOfficer.innerHTML += listItem;
+            cursor.continue();
+        }
+    }
+
+}
+
+function displayProfile(){
+    let companyStore = DB.transaction(["companies"]).objectStore("companies");
+    companyStore.openCursor().onsuccess = function(e){
+        let cursor = e.target.result;
+        if(cursor){       
+            if( cursor.value.email == keyEmail.slice(1,keyEmail.length-1)){
+               
+                companyText.appendChild(document.createTextNode(cursor.value.name));
+                emailText.appendChild(document.createTextNode(cursor.value.email));
+                locationText.appendChild(document.createTextNode(cursor.value.latitude + ", "+ cursor.value.longitude));
+                chargeText.appendChild(document.createTextNode(cursor.value.charge + " Birr"));
+                slotsText.appendChild(document.createTextNode(cursor.value.slots));
+                return;
+            }
+            
+            cursor.continue();
+        }
+       
+    }
+}
+
+formUpdate.addEventListener('submit',updateProfile);
+
+function updateProfile(){
+    let companyStore = DB.transaction(["companies"],"readwrite").objectStore("companies");
+    let request = companyStore.get(keyEmail);
+    
+
+    request.onsuccess = function(){
+        let updateData = {
+            name: companyInput.value,
+            email: keyEmail.slice(1,keyEmail.length-1),
+            opens_at: 8,
+            closes_at: 8,
+            password: "We respect privacy",
+            charge: chargeInput.value,
+            slots: slotsInput.value,
+            active_slots:"",
+            latitude: latitudeInput.value,
+            longitude: longtiudeInput.value
+            
+        }
+        let updateTable = companyStore.put(updateData);
+        updateTable.onsuccess = function(){
+            location.reload();
+            console.log("done")
+        }
+    }
+}
+
 
 // parking officer part
 
@@ -38,32 +179,5 @@ cancelIcon.addEventListener('click',function(){
     document.getElementById("add-btn").removeAttribute("disabled");
 });
 
-form.addEventListener('submit', add_parking_officer)
-var i = 0;
-function add_parking_officer(e){
-    e.preventDefault(); 
-    i++;
-    form.style.display = "none";
-    document.getElementById("add-btn").removeAttribute("disabled");
-    display_parking_officer();
 
-}
-
-function display_parking_officer(){
-    console.log(i);
-    let listItem = `
-            <ul class="list-inline row list-item">
-                <li class="col-1 ml-0 pl-5 ">
-                    <input class="input-group" type="checkbox" value="" id="selectAll" />
-                </li>
-                <li class="col-2">Mintesnot Bezabhi</li>
-                <li class="col-2 ">0924316299</li>
-                <li class="col-3">mintesnotbezabhi99@gmail.com</li>
-                <li class="col-2">Male</li>
-                <li class="col-2 ">
-                    <i class="fa fa-remove mr-2"></i>  &nbsp;<a href="#"><i class="fa fa-edit"></i> </a>
-                </li>
-            </ul>`;
-            parkingOfficer.innerHTML += listItem;
-}
 
